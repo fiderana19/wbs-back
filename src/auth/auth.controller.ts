8a1915e94ab16004de69f78bfeb9f0a2df79a435
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-01-30 21:11:42 +0300

    feat: auth guard almost done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/dto/create-user.dto';
+import { LoginDto } from 'src/dto/login.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -9,12 +11,18 @@ export class AuthController {
     ) {}
 
     @Get('/all')
+    @UseGuards(AuthGuard())
     async getAllUser() {
         return await this.authService.getAllUser();
     }
 
     @Post('/create')
-    async createUser(@Body() createUserDto) {        
+    async createUser(@Body() createUserDto: CreateUserDto) {        
         return await this.authService.createUser(createUserDto);
+    }
+
+    @Post('/login')
+    async loginUser(@Body() loginDto: LoginDto) {
+        return await this.authService.login(loginDto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,14 +2,18 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from 'src/dto/create-user.dto';
+import { LoginDto } from 'src/dto/login.dto';
 import { User } from 'src/schema/user.schema';
 import { generateRandomRef } from 'src/utils/generateRandom';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectModel(User.name)
         private userModel: Model<User>,
+        private jwtService: JwtService,
     ) {}
 
     async getAllUser() {
@@ -18,18 +22,36 @@ export class AuthService {
 
     async createUser(createUserData: CreateUserDto) {
         const key = generateRandomRef();
+        const hashedPassword = await bcrypt.hash(createUserData.password, 10);
 
         await this.userModel.create({
             usrid: key,
             username: createUserData.username,
-            password: createUserData.password
+            password: hashedPassword
         })
 
         return key;
     }
 
-    async signup(signupData) {
+    async login(loginData: LoginDto) {
+        const { usrid, password } = loginData;
+        const user = await this.userModel.findOne({ usrid });
 
+        if(!user) {
+            throw new UnauthorizedException("User not found !");
+        }
+
+        const isPasswordMatched = await bcrypt.compare(password,user.password);
+
+        if(!isPasswordMatched) {
+            throw new UnauthorizedException("Invalid password");
+        }
+
+        const access_token = await this.jwtService.sign({
+            id: user._id
+        })
+
+        return { token: access_token }
     }
 
 }
